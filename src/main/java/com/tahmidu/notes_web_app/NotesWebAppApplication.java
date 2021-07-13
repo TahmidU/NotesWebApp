4616ee5d48830eefc6dee9b4e878af5607521006
TahmidU <tahmiduddin@hotmail.co.uk>
2021-07-13 19:42:20 +0100

    - New: Initial commit.

---
+++ src/main/java/com/tahmidu/notes_web_app/NotesWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.tahmidu.notes_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesWebAppApplication.class, args);
+	}
+
+}
