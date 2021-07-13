b5183b50f413dcb7666cb38553bf014c8723dd8c
TahmidU <tahmiduddin@hotmail.co.uk>
2021-07-13 19:51:26 +0100

    - New: Model finished. - New: H2 database configured and created.

---
+++ src/main/java/com/tahmidu/notes_web_app/model/Note.java
@@ -0,0 +1,20 @@
+package com.tahmidu.notes_web_app.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Table(name = "note")
+@Entity
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "note_id", nullable = false)
+    private Long noteId;
+
+    @Column(name = "title", nullable = false) private String Title;
+    @Column(name = "content", nullable = false) private String content;
+
+}
